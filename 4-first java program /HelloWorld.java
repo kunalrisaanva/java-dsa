70da2c293c5d88aba5655aff8c48cb0ca99a869d
kunal <kunalrisaanva12@gmail.com>
2024-04-20 11:12:13 +0530

    initalize folder structure

---
+++ 4-first java program /HelloWorld.java
@@ -0,0 +1,32 @@
+// Output
+// Varibles 
+// Data Types
+// Input
+
+
+
+
+// output in java  ----- 
+
+
+
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+
+
+
+// import java.util.*;
+
+// public class HelloWorld {
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in) ;
+//         String name = sc.next();
+//         System.out.println(name);
+//     }
+// }
---
+++ 4-first java program /README.md
@@ -0,0 +1,7 @@
+# Java Notes 
+
+* Output in Java
+* Variables in Java
+* Data-Types in Java
+* input in Java
+
